118c8b50a87b54fb3edafda93808127a4bcdd3a8
sejal0699 <sejal.srivastava@apinventiv.com>
2024-09-24 11:59:56 +0530

    new commit

--- src/components/AccountLockedModal.js
+++ src/components/AccountLockedModal.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Modal, View, Text, TouchableOpacity, StyleSheet,Image } from 'react-native';
 
 
+
 const AccountLockedModal = ({ visible, closeModal }) => {
   return (
     <Modal
